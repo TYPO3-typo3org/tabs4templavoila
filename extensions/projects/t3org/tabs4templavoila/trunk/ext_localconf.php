57b4cd4188c1b34fad2c64f2720f183fd8084385
tolleiv.nietsch <tolleiv.nietsch@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2011-04-21 10:06:26 +0000

    tabs4templavoila initial

---
+++ extensions/projects/t3org/tabs4templavoila/trunk/ext_localconf.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (TYPO3_MODE == 'BE') {
+
+t3lib_extMgm::addPageTSConfig('
+mod.web_txtemplavoilaM1.javascript {  
+	jquery = EXT:tabs4templavoila/res/js/jquery-1.4.2.min.js 
+	jquerycookie = EXT:tabs4templavoila/res/js/jquery.cookie.js
+	jqueryui = EXT:tabs4templavoila/res/js/jquery-ui-1.8.2.custom.min.js
+	applytabs = EXT:tabs4templavoila/res/js/applyjquerytabs.js
+}
+
+
+mod.web_txtemplavoilaM1.stylesheet {  
+	jqueryuicss = EXT:tabs4templavoila/res/css/black-tie/jquery-ui-1.8.2.custom.css
+}
+');
+
+
+}
+?>
